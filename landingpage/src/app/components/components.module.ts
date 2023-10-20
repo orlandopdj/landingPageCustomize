47a4e9353cac865827a92a8588c5b998cc0fd5f7
Jocimar Lopes <usuario@MacBook-Air-de-Usuario.local>
2023-10-20 20:46:51 -0300

    feature/iniciado_projeto

---
+++ landingpage/src/app/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    HeaderComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
+})
+export class ComponentsModule { }
